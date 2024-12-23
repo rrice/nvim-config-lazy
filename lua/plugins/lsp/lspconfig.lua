7686e7cd9c4636b57c56e1c25269477471a0f7c8
Ralph Allan Rice <ralph.rice@gmail.com>
2024-12-22 21:13:01 -0500

    Merged mason.lua and lspconfig.lua.

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -42,4 +42,30 @@ return {
       })
     end,
   },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      ensure_installed = {
+        "prettier",
+      },
+      setup = {
+        rust_analyzer = function()
+          return true
+        end,
+      },
+      servers = {
+        lua_ls = {
+          mason = false,
+        },
+      },
+    },
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
