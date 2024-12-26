730790f5014a906c2a60e06be24434ce640dd581
Ralph Allan Rice <ralph.rice@gmail.com>
2024-12-25 22:48:57 -0500

    Rewrote and streamlined lspconfig.lua

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,71 +1,113 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "williamboman/mason.nvim", config = true },
     "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    config = function()
-      local masonlsp = require("mason-lspconfig")
-
-      local disabled_servers = {}
-
-      masonlsp.setup({
-        ensure_installed = { "html", "gopls", "ts_ls" },
-        automatic_installation = true,
-      })
-      masonlsp.setup_handlers({
-        function(server)
-          -- Ignore servers on the disabled_servers list.
-          for _, name in pairs(disabled_servers) do
-            if name == server then
-              return
-            end
-          end
-          local opts = {
-            -- Add global options in either directly here or separate Lua
-            -- module, then hook it up here.
-            -- on_attach = on_attach,
-            -- capabilities = capabilities
-          }
-
-          local require_ok, found_server = pcall(require, "plugins.lsp." .. server)
-          if require_ok then
-            opts = vim.tbl_deep_extend("force", found_server, opts)
-          end
-          require("lspconfig")[server].setup(opts)
-        end,
-      })
-    end,
+    "hrsh7th/cmp-nvim-lsp",
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      ensure_installed = {
-        "prettier",
-      },
-      setup = {
-        rust_analyzer = function()
-          return true
-        end,
-      },
-      servers = {
-        lua_ls = {
-          mason = false,
+  config = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("rice-lsp-attach", { clear = true }),
+      callback = function(event)
+        local map = function(keys, fn, desc, mode)
+          mode = mode or "n"
+          vim.keymap.set(mode, keys, fn, { buffer = event.buf, desc = "LSP: " .. desc })
+        end
+        local tbn = require("telescope.builtin")
+
+        -- Jump to the definition of the word under the cursor, use <C-t> to get back.
+        map("gd", tbn.lsp_definitions, "[G]oto [D]efinition")
+
+        -- Find the references for the word under the cursor.
+        map("gr", tbn.lsp_references, "[G]oto [R]eferences")
+
+        -- Jump to the implementation of the word under the cursor.
+        map("gI", tbn.lsp_implementations, "[G]oto [I]mplementation")
+
+        -- Jump to the type of the word under the cursor.
+        map("<leader>D", tbn.lsp_type_definitions, "Goto Type [D]efinition")
+
+        -- Fuzzy find all the symbols in the current document.
+        map("<leader>ds", tbn.lsp_document_symbols, "[D]ocument [S]ymbols")
+
+        -- Fuzzy find all the symbols in the current workspace.
+        map("<leader>ws", tbn.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+        -- Rename the symbol under the cursor.
+        map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+
+        -- Execute a code action, usually the cursor has to be on top of an error
+        -- or the suggestion to be activated.
+        map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
+
+        -- Jump to the declaration of the word under the cursor.
+        map("<leader>gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+        -- Use CursorHold to highlight references of the word under the cursor after
+        -- the cursor lands there and rests.
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          local highlight_group = vim.api.nvim_create_augroup("rice-lsp-highlight", { clear = false })
+          vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+            buffer = event.buf,
+            group = highlight_group,
+            callback = vim.lsp.buf.document_highlight,
+          })
+          vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+            buffer = event.buf,
+            group = highlight_group,
+            callback = vim.lsp.buf.clear_references,
+          })
+          vim.api.nvim_create_autocmd("LspDetach", {
+            group = vim.api.nvim_create_augroup("rice-lsp-detach", { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds({ group = "rice-lsp-highlight", buffer = event2.buf })
+            end,
+          })
+        end
+
+        -- Toggle inlay hints
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+          map("<leader>th", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+          end, "[T]oggle Inlay [H]ints")
+        end
+      end,
+    })
+    -- Merge capabilities from nvim-cmp and broadcast it to the LSP servers.
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+    -- LSP servers to add.
+    --
+    -- Keys -
+    --   cmd (table): override the default command used to start the server.
+    --   filetypes (table): override the default list of associated filetypes used by the server.
+    --   capabilities (table): override fields in the capabilities.
+    --   settings (table): override the default settings that are passed in when initializing the server.
+    local servers = {
+      lua_ls = {
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = "Replace",
+            },
+          },
         },
       },
-    },
-    lazy = false,
-    config = function()
-      local lspconfig = require("lspconfig")
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+    }
+
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      handlers = {
+        function(name)
+          local server = servers[name] or {}
+          -- Handles only overriding the servers defined above.
+          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+          require("lspconfig")[name].setup(server)
+        end,
+      },
+    })
+  end,
 }
